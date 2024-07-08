3147d43decbb183325acfa4f1c4089f16aa86e9f
Alex Lazcano <alazcano2000@gmail.com>
2024-07-08 16:12:03 -0700

    First Commit

---
+++ src/main/java/com/example/ocr_receipt/OcrReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.example.ocr_receipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrReceiptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrReceiptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ocr_receipt/OcrReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocr_receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrReceiptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
